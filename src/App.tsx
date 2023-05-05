4f34c4c62bf3116f8a5ee03087e10203a949c682
Mishratapas <mishratapas769@gmail.com>
2023-05-05 18:50:14 +0530

    angular lawda

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ReactAPP from './react_v/src/ReactAPP.tsx'
+//import {AppComponent} from  './angular_v/src/app/app.component'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isReact, setIsReact] = useState(true);
+
+  const handleClick = () => {
+    setIsReact(!isReact);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+<button onClick={handleClick}> {isReact ? 'View Angular version' : 'View React version'}</button>
+
+<div>{isReact ? <ReactAPP /> : 'Angular'}</div>
     </>
   )
 }
---
+++ src/react_v/src/ReactAPP.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function ReactAPP() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+React version re lawde
+    </>
+  )
+}
+
+export default ReactAPP
